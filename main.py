f87678f0315c32e779623e4a343784c775696f17
Quasaarr <97186363+Quasaarr@users.noreply.github.com>
2022-01-06 11:20:27 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 while True:
-    try:
-        speed = int(input("Speed? "))
-    except ValueError:
-        print("Numerical Values ONLY!")
-        speed = int(input("Speed? "))
-    unit = input("(M)ph or (K)ph? ")
+    # Speed-Input Loop
+    while True:
+        try:
+            speed = int(input("Speed? "))
+            break
+        except ValueError:
+            print("Numerical Values ONLY!")
+
+    # Unit-Input Loop
+    while True:
+       unit = input("(M)ph or (K)ph? ")
+       if unit.upper() in ("M", "K"):
+           break
+       print("Please input (M)ph or (K)ph!")
+       continue
     if unit.upper() == "M":
         converted_speed = speed * 1.60934
         print(f"You were going {converted_speed}kph!")
-    elif unit.upper() == "K":
+    if unit.upper() == "K":
         converted_speed = speed / 1.60934
         print(f"You were going {converted_speed}mph!")
-    else:
-        print("Please input (M)ph or (K)ph!")
+
+    # Repeat-Input Loop
     while True:
         answer = str(input("Run again? (Y/N): "))
         if answer.upper() in ("Y", "N"):
