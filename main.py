09bde999fce840427d961e757682b67774036f52
Quasaarr <97186363+Quasaarr@users.noreply.github.com>
2022-01-08 01:06:30 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -20,40 +20,40 @@ while True:
 
 # Desired_Unit-Input Loop
     while True:
-       desired_unit = input("What would you like to convert to? (M)ph or (K)m/h or (KN)ots? ")
-       if desired_unit.upper() in ("M", "K", "KN"):
-           break
-       print("Please input (M)ph or (K)m/h or (KN)ots!", line_break)
-       continue
+        desired_unit = input("What would you like to convert to? (M)ph or (K)m/h or (KN)ots? ")
+        if desired_unit.upper() in ("M", "K", "KN"):
+            break
+        print("Please input (M)ph or (K)m/h or (KN)ots!", line_break)
+        continue
 
 # Convert and Print Loop
     if unit.upper() == "M":
         if desired_unit.upper() == "K":
             converted_speed = speed * 1.6093442101
-            print(f"You were going {converted_speed}km/h!", line_break)
+            print(f"You were going {converted_speed.__round__(5)}km/h!", line_break)
         if desired_unit.upper() == "KN":
             converted_speed = speed * 0.868976823
-            print(f"You were going {converted_speed} knots!", line_break)
+            print(f"You were going {converted_speed.__round__(5)} knots!", line_break)
         if unit.upper() == desired_unit.upper():
-            print(f"You were going {speed}mph!", line_break)
+            print(f"You were going {speed.__round__(5)}mph!", line_break)
     if unit.upper() == "K":
         if desired_unit.upper() == "M":
             converted_speed = speed * 0.6213711111
-            print(f"You were going {converted_speed}mph!", line_break)
+            print(f"You were going {converted_speed.__round__(5)}mph!", line_break)
         if desired_unit.upper() == "KN":
             converted_speed = speed * 0.5399569444
-            print(f"You were going {converted_speed} knots!", line_break)
+            print(f"You were going {converted_speed.__round__(5)} knots!", line_break)
         if unit.upper() == desired_unit.upper():
-            print(f"You were going {speed}km/h!", line_break)
+            print(f"You were going {speed.__round__(5)}km/h!", line_break)
     if unit.upper() == "KN":
         if desired_unit.upper() == "M":
             converted_speed = speed * 1.1507789973
-            print(f"You were going {converted_speed}mph!", line_break)
+            print(f"You were going {converted_speed.__round__(5)}mph!", line_break)
         if desired_unit.upper() == "K":
             converted_speed = speed * 0.0005144443
-            print(f"You were going {converted_speed}km/h!", line_break)
+            print(f"You were going {converted_speed.__round__(5)}km/h!", line_break)
         if unit.upper() == desired_unit.upper():
-            print(f"You were going {speed} knots!", line_break)
+            print(f"You were going {speed.__round__(5)} knots!", line_break)
 
     # Repeat-Input Loop
     while True:
@@ -66,3 +66,7 @@ while True:
     if answer.upper() == "N":
         print("Goodbye! Thank you for using this program!")
         break
+
+
+
+
