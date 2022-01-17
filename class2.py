9e264fe134a94a0c619e129bebe225c43a073b99
Quasaarr <97186363+Quasaarr@users.noreply.github.com>
2022-01-16 17:23:26 -0800

    Add files via upload

--- class2.py
+++ class2.py
@@ -1,40 +1,53 @@
+import config
+speed = config.Speed()
+accuracy = config.Accuracy()
+desired_unit = config.Desired_Unit()
+
 class ConvertSpeedKN:
-    def KNtKN(self):
-        print(f"You were going {speed.__round__(accuracy)} knots!", "\n")
+    def kn_t_kn(self):
+        if desired_unit.upper() == "KN":
+            print(f"You were going {speed.__round__(accuracy)} knots!", "\n")
 
-    def KNtM(self):
-        FeetH = speed * 6076
-        outcome = FeetH / 5280
-        print(f"You were going {outcome.__round__(accuracy)}mph!", "\n")
+    def kn_t_m(self):
+        if desired_unit.upper() == "M":
+            feet_h = speed * 6076
+            outcome = feet_h / 5280
+            print(f"You were going {outcome.__round__(accuracy)}mph!", "\n")
 
-    def KNtK(self):
-        FeetH = speed * 6076
-        MetersH = FeetH * 0.3048
-        outcome = MetersH / 1000
-        print(f"You were going {outcome.__round__(accuracy)}km/h!", "\n")
+    def kn_t_k(self):
+        if desired_unit.upper() == "K":
+            feet_h = speed * 6076
+            meters_h = feet_h * 0.3048
+            outcome = meters_h / 1000
+            print(f"You were going {outcome.__round__(accuracy)}km/h!", "\n")
 
-    def KNtMA(self):
-        outcome = speed / 661.7
-        print(f"You were going {outcome.__round__(accuracy)} mach (at sea level)!", "\n")
+    def kn_t_ma(self):
+        if desired_unit.upper() == "MA":
+            outcome = speed / 661.7
+            print(f"You were going {outcome.__round__(accuracy)} mach (at sea level)!", "\n")
 
 
 class ConvertSpeedMA:
-    def MAtMA:
-        print(f"You were going {speed.__round__(accuracy)} mach (at sea level)!", "\n")
+    def ma_t_ma:
+        if desired_unit.upper() == "MA":
+            print(f"You were going {speed.__round__(accuracy)} mach (at sea level)!", "\n")
 
-    def MAtM:
-        KnotsH = speed * 661.7
-        FeetH = KnotsH * 6076
-        outcome = FeetH / 5280
-        print(f"You were going {outcome.__round__(accuracy)}mph!", "\n")
+    def ma_t_m:
+        if desired_unit.upper() == "M":
+            knots_h = speed * 661.7
+            feet_h = knots_h * 6076
+            outcome = feet_h / 5280
+            print(f"You were going {outcome.__round__(accuracy)}mph!", "\n")
 
-    def MAtK:
-        KnotsH = speed * 661.7
-        FeetH = KnotsH * 6076
-        MetersH = FeetH * 0.3048
-        outcome = MetersH / 1000
-        print(f"You were going {outcome.__round__(accuracy)}km/h!", "\n")
+    def ma_t_k:
+        if desired_unit.upper() == "K":
+            knots_h = speed * 661.7
+            feet_h = knots_h * 6076
+            meters_h = feet_h * 0.3048
+            outcome = meters_h / 1000
+            print(f"You were going {outcome.__round__(accuracy)}km/h!", "\n")
 
-    def MAtKN:
-        outcome = speed * 661.7
-        print(f"You were going {outcome.__round__(accuracy)} knots!", "\n")
+    def ma_t_kn:
+        if desired_unit.upper() == "KN":
+            outcome = speed * 661.7
+            print(f"You were going {outcome.__round__(accuracy)} knots!", "\n")
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from class1 import ConvertSpeedM
+from class1 import ConvertSpeedK
+from class2 import ConvertSpeedKN
+from class2 import ConvertSpeedMA
+from config import Speed
+from config import Accuracy
+from config import Desired_Unit
+
+
 line_break = "\n"
 
 # Program Introduction
@@ -9,90 +18,41 @@ All conversion formulas are from NASA's official website!
 
 # Program Loop
 while True:
-    # Speed-Input Loop
-    while True:
-        try:
-            speed = float(input("Speed? "))
-            break
-        except ValueError:
-            print("Please input numerical values only!", line_break)
+    Speed()
 
 # Unit-Input Loop
     while True:
-        unit = input("What unit of speed? (M)ph, (K)m/h, (Kn)ots, or (Ma)ch? ")
+        unit = str(input("What unit of speed? (M)ph, (K)m/h, (Kn)ots, or (Ma)ch? "))
         if unit.upper() in ("M", "K", "KN", "MA"):
             break
         print("Please input (M)ph, (K)m/h, (Kn)ots, or (Ma)ch!", line_break)
         continue
 
 # Desired_Unit-Input Loop
-    while True:
-        desired_unit = input("What would you like to convert to? (M)ph, (K)m/h, (Kn)ots, or (Ma)ch? ")
-        if desired_unit.upper() in ("M", "K", "KN", "MA"):
-            break
-        print("Please input (M)ph, (K)m/h, (Kn)ots, or (Ma)ch!", line_break)
-        continue
+    Desired_Unit()
 
-# Accuracy-Input Loop
-    while True:
-        try:
-            accuracy = int(input("How many decimal points would you like to round to? "))
-            break
-        except ValueError:
-            print("Please input integers (whole numbers) only!", line_break)
+    Accuracy()
 
-# Convert and Print Loop
     if unit.upper() == "M":
-        if desired_unit.upper() == "K":
-            converted_speed = speed * 1.6093442101
-            print(f"You were going {converted_speed.__round__(accuracy)}km/h!", line_break)
-        elif desired_unit.upper() == "KN":
-            converted_speed = speed * 0.868976823
-            print(f"You were going {converted_speed.__round__(accuracy)} knots!", line_break)
-        elif unit.upper() == desired_unit.upper():
-            print(f"You were going {speed}mph!", line_break)
-        elif desired_unit.upper() == "MA":
-            converted_speed = speed / 767.269148
-            print(f"You were going {converted_speed.__round__(accuracy)} mach!", line_break)
-
+        class1.ConvertSpeedM.m_t_m()
+        class1.ConvertSpeedM.m_t_k()
+        class1.ConvertSpeedM.m_t_kn()
+        class1.ConvertSpeedM.m_t_ma()
     if unit.upper() == "K":
-        if desired_unit.upper() == "M":
-            converted_speed = speed * 0.6213711111
-            print(f"You were going {converted_speed.__round__(accuracy)}mph!", line_break)
-        elif desired_unit.upper() == "KN":
-            converted_speed = speed * 0.5399569444
-            print(f"You were going {converted_speed.__round__(accuracy)} knots!", line_break)
-        elif unit.upper() == desired_unit.upper():
-            print(f"You were going {speed}km/h!", line_break)
-        elif desired_unit.upper() == "MA":
-            converted_speed = speed / 1234.8
-            print(f"You were going {converted_speed.__round__(accuracy)} mach!", line_break)
-
+        class1.ConvertSpeedK.k_t_k()
+        class1.ConvertSpeedK.k_t_m()
+        class1.ConvertSpeedK.k_t_kn()
+        class1.ConvertSpeedK.k_t_ma()
     if unit.upper() == "KN":
-        if desired_unit.upper() == "M":
-            converted_speed = speed * 1.1507789973
-            print(f"You were going {converted_speed.__round__(accuracy)}mph!", line_break)
-        elif desired_unit.upper() == "K":
-            converted_speed = speed * 0.0005144443
-            print(f"You were going {converted_speed.__round__(accuracy)}km/h!", line_break)
-        elif unit.upper() == desired_unit.upper():
-            print(f"You were going {speed} knots!", line_break)
-        elif desired_unit.upper() == "MA":
-            converted_speed = speed / 666.738661
-            print(f"You were going {converted_speed.__round__(accuracy)} mach!", line_break)
-
+        class2.ConvertSpeedKN.kn_t_kn()
+        class2.ConvertSpeedKN.kn_t_m()
+        class2.ConvertSpeedKN.kn_t_k()
+        class2.ConvertSpeedKN.kn_t_ma()
     if unit.upper() == "MA":
-        if desired_unit.upper() == "M":
-            converted_speed = speed * 767.269148
-            print(f"You were going {converted_speed.__round__(accuracy)}mph!", line_break)
-        elif desired_unit.upper() == "K":
-            converted_speed = speed * 1234.8
-            print(f"You were going {converted_speed.__round__(accuracy)}km/h!", line_break)
-        elif desired_unit.upper() == "KN":
-            converted_speed = speed * 661.738661
-            print(f"You were going {converted_speed.__round__(accuracy)} knots!")
-        elif unit.upper() == desired_unit.upper():
-            print(f"You were going {speed} mach!", line_break)
+        class2.ConvertSpeedMA.ma_t_ma()
+        class2.ConvertSpeedMA.ma_t_m()
+        class2.ConvertSpeedMA.ma_t_k()
+        class2.ConvertSpeedMA.ma_t_kn()
 
 # Repeat-Input Loop
     while True:
