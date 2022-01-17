96de7999b9bb83abdf2cfb158e77f3db08a2176e
Quasaarr <97186363+Quasaarr@users.noreply.github.com>
2022-01-16 23:49:11 -0800

    Add files via upload

---
+++ ConversionFunctions/KnotConversion.py
@@ -0,0 +1,22 @@
+class ConvertSpeedKN:
+    def kn_t_kn(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "KN":
+            return speed.__round__(accuracy)
+
+    def kn_t_m(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "M":
+            feet_h = speed * 6076
+            outcome = feet_h / 5280
+            return outcome.__round__(accuracy)
+
+    def kn_t_k(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "K":
+            feet_h = speed * 6076
+            meters_h = feet_h * 0.3048
+            outcome = meters_h / 1000
+            return outcome.__round__(accuracy)
+
+    def kn_t_ma(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "MA":
+            outcome = speed / 661.7
+            return outcome.__round__(accuracy)
---
+++ ConversionFunctions/MachConversion.py
@@ -0,0 +1,24 @@
+class ConvertSpeedMA:
+    def ma_t_ma(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "MA":
+            return speed.__round__(accuracy)
+
+    def ma_t_m(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "M":
+            knots_h = speed * 661.7
+            feet_h = knots_h * 6076
+            outcome = feet_h / 5280
+            return outcome.__round__(accuracy)
+
+    def ma_t_k(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "K":
+            knots_h = speed * 661.7
+            feet_h = knots_h * 6076
+            meters_h = feet_h * 0.3048
+            outcome = meters_h / 1000
+            return outcome.__round__(accuracy)
+
+    def ma_t_kn(self, speed, desired_unit, accuracy):
+        if desired_unit.upper() == "KN":
+            outcome = speed * 661.7
+            return outcome.__round__(accuracy)
